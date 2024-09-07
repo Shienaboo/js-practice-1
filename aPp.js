c16922a6ed1702748314d0b2cb632d6c5b7be462
Janadriel <pjanadriel@gmail.com>
2024-09-07 22:26:15 +0800

    Create aPp.js

---
+++ aPp.js
@@ -0,0 +1,24 @@
+console.log("Using numbers");
+const a = ;
+const b = ;
+
+const sum = x + y;
+console.log(sum);
+
+console.log('Using Booleans');
+let c = true;
+let d = false;
+console.log("I am learning programming", d);
+
+console.log('Comparison Operators Section');
+let e = ;
+let f = ;
+
+let g = e > f;
+console.log("Is e more than f? "+g);
+let h = e < f;
+console.log("Is e less than f? "+ h);
+let i = e === f;
+console.log("Is e equal to f? "+i);
+let j = e !=f;
+console.log("Is e not equal to f? "+ j);
